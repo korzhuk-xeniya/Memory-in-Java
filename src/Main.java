216ad1729de763c98a894f5c3cd853a3057ba8a9
Ксения <korzhuk.xeniya@yandex.ru>
2023-07-01 09:28:15 +0300

    task 6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    public static Integer chargeValue(Integer value) {
+        value = 22;
+        return value;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer value = 33;
+        chargeValue(value);
+        System.out.println(value);
+        System.out.println(chargeValue(value));
     }
 }
