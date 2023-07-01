a7ad1c1e44ecd704848cebe48285eb2ef3d53234
Ксения <korzhuk.xeniya@yandex.ru>
2023-07-01 09:45:17 +0300

    task 7

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
-    public static Integer chargeValue(Integer value) {
-        value = 22;
+    public static Integer[] changeValue(Integer [] value) {
+        value = new Integer[]{1, 2};
         return value;
     }
 
     public static void main(String[] args) {
-        Integer value = 33;
-        chargeValue(value);
-        System.out.println(value);
-        System.out.println(chargeValue(value));
+        Integer [] value = new Integer[]{3,4};
+        changeValue(value);
+        System.out.println(Arrays.toString(value));
+        System.out.println(Arrays.toString(changeValue(value)));
     }
 }
